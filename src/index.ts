3de6d492fdb6e4aa177b4e62751ab47f59b4f0b7
takashi <tks.srtr89@gmail.com>
2023-07-30 15:35:36 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { COLOR_MAP_KEYS } from './common/const'
 ;(() => {
   program
     .description('Split a string into substrings and display as an array')
-    .option('-w, --word <char>', 'somthing word')
+    .option('-w, --word <char>', 'somthing display word')
     .option('-c, --color <char>', `colorKey [${COLOR_MAP_KEYS.toString()}]`)
 
   const parsed = program.parse(process.argv)

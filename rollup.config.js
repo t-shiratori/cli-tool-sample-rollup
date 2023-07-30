e417b708d1d1d2c94524d082f6d9d055bae40259
takashi <tks.srtr89@gmail.com>
2023-07-30 14:34:08 +0900

    feat: enable commander pkg

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
-import shebangPlugin from './rollup-plugins/rollup-plugin-shebang.js'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import shebangPlugin from './rollup-plugins/rollup-plugin-shebang.js'
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: 'dist/index.js',
-      format: 'iife',
+      format: 'es',
     },
     {
       file: 'dist/index.min.js',
-      format: 'iife',
+      format: 'es',
       plugins: [terser()],
     },
   ],
-  plugins: [typescript(), shebangPlugin()],
+  plugins: [typescript(), nodeResolve(), commonjs(), shebangPlugin()],
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import { program } from 'commander'
 import { colorPrint } from './common/utils'
+
+program.description('Split a string into substrings and display as an array').option('-w, --words <char>', 'words')
+
+const parsed = program.parse(process.argv)
+
+console.log({ 'process.argv': process.argv, parsed })
 
 const speaker = () => {
   colorPrint({ words: 'hoge' })
